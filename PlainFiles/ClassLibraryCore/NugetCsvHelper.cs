b147535afc17085aba7d401cc4dd33cd0767fdd2
SANTIAGO HERRERA MUNERA <santiagoherrera1141378@correo.itm.edu.co>
2025-11-23 21:05:51 -0500

    Users and people are created

---
+++ PlainFiles/ClassLibraryCore/NugetCsvHelper.cs
@@ -0,0 +1,41 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace PlainFiles.Core;
+
+public class NugetCsvHelper
+{
+    public void Write(string path, IEnumerable<Person> people)
+    {
+        using var sw = new StreamWriter(path);
+        using var cw = new CsvWriter(sw, CultureInfo.InvariantCulture);
+        cw.WriteRecord(people);
+    }
+
+    public IEnumerable<Person> ReadPersons(string path)
+    {
+        if (!File.Exists(path))
+            return Enumerable.Empty<Person>();
+
+        using var sr = new StreamReader(path);
+        using var cr = new CsvReader(sr, CultureInfo.InvariantCulture);
+        return cr.GetRecords<Person>().ToList();
+    }
+
+    public void WriteUsers(string path, IEnumerable<User> Users)
+    {
+        using var sw = new StreamWriter(path);
+        using var cw = new CsvWriter(sw, CultureInfo.InvariantCulture);
+        cw.WriteRecord(Users);
+    }
+
+    public IEnumerable<User> ReadUsers(string path)
+    {
+        if (!File.Exists(path))
+            return Enumerable.Empty<User>();
+
+        using var sr = new StreamReader(path);
+        using var cr = new CsvReader(sr, CultureInfo.InvariantCulture);
+        return cr.GetRecords<User>().ToList();
+    }
+}
